f8206c9e80fa58bfe4707d4d5f583568acd45ba3
berkaygulyaz <40126852+berkaygulyaz@users.noreply.github.com>
2021-02-06 13:42:12 +0300

    Site design done

---
+++ todo-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>To-Do List</h1>
+      </header>
+
+      <Form />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
