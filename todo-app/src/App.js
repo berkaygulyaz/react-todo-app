946341face563be96f0ee97c19ca6bfeacba845e
berkaygulyaz <40126852+berkaygulyaz@users.noreply.github.com>
2021-02-08 20:45:28 +0300

    Todo items listed and events written

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
       </header>
 
       <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
-      <TodoList />
+      <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Todo from './Todo';
 
-const TodoList = () => {
+const TodoList = ({ todos, setTodos }) => {
     return (
         <div className="todo-container">
             <ul className="todo-list">
-
+                {todos.map(todo => (
+                    <Todo setTodos={setTodos} todos={todos} todo={todo} text={todo.text} key={todo.id} />
+                ))}
             </ul>
         </div>
     )
